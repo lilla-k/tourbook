1892a3069ae1f1a93301362bb0a1cd07af33fd2f
lilla-k <kalmarlilianna@gmail.com>
2024-01-21 09:17:01 +0100

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header/Header.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <header className="App-header">
+        Hello
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,8 @@
+
+function Header(){
+    return(
+        <h1>Tourbook</h1>
+    )
+}
+
+export default Header
