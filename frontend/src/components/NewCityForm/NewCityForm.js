088658b3f28c04ba24daa098d41c2bf8a4210e28
lilla-k <kalmarlilianna@gmail.com>
2024-03-15 02:43:16 +0100

    new attraction form with add button

--- frontend/src/components/NewCityForm/NewCityForm.js
+++ frontend/src/components/NewCityForm/NewCityForm.js
@@ -1,5 +1,6 @@
 import { useParams, useOutletContext, useNavigate } from "react-router-dom";
 import { useState } from 'react';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import './NewCityForm.css';
@@ -14,6 +15,9 @@ function NewCityForm() {
 
     const [cityInformation, setCityInformation] = useState("");
     const [cityName, setCityName] = useState("");
+    const [attractions, setAttractions] = useState([""]);
+
+
 
     async function postCityData() {
         const cityData = {
@@ -25,32 +29,33 @@ function NewCityForm() {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(cityData)
         })
-        if (response.status === 201){
+        if (response.status === 201) {
             const pushNewCity = () => {
                 console.log(trips)
                 trips.forEach(trip => {
-                    if(trip.id === tripId){
+                    if (trip.id === tripId) {
                         trip.visitedCities.push(cityData);
                     }
                 })
                 console.log(trips);
                 return trips;
             }
-           
-           setTrips(pushNewCity);
-           navigate(`/trips/${tripId}`)
+
+            setTrips(pushNewCity);
+            navigate(`/trips/${tripId}`)
         }
     }
+    console.log(attractions);
 
-    return(<div className = "NewCityForm" >
+    return (<div className="NewCityForm" >
         <div className="NewCityForm-title">Information about the city in {selectedTrip.country}</div>
         <div className="NewCityForm-form">
             <div className="NewCityForm-cityName">
-                <TextField 
-                label="City name" 
-                variant="outlined" 
-                value={cityName}
-                onChange={e => setCityName(e.target.value)}
+                <TextField
+                    label="City name"
+                    variant="outlined"
+                    value={cityName}
+                    onChange={e => setCityName(e.target.value)}
                 />
             </div>
             <div className="NewCityForm-cityInformation">
@@ -64,14 +69,33 @@ function NewCityForm() {
                 />
             </div>
             <div className="NewCityForm-attractions">
-                <div>You can add the visited attractions:</div>
+                {attractions.map((attraction, index) => {
+                    return (
+                        <div>
+                            <TextField 
+                                label="Visited attraction" 
+                                variant="outlined"
+                                value={attraction}
+                                onChange={e => {
+                                    attractions[index]= e.target.value;
+                                    setAttractions([...attractions]);
+                                }}
+                            />
+                            {attractions.length===(index+1) &&  < AddCircleOutlineIcon 
+                                className="NewCityForm-addAttractionBtn"
+                                    onClick={e=>setAttractions([...attractions, ""])} 
+                                /> 
+                            }
+                        </div>
+                    )
+                })}
             </div>
             <div className="NewCityForm-saveButton">
-                    <Button variant="outlined"onClick={() => postCityData()} >Upload details</Button>
+                <Button variant="outlined" onClick={() => postCityData()} >Upload details</Button>
             </div>
         </div>
 
     </div >)
-    }
+}
 
-    export default NewCityForm;
+export default NewCityForm;
