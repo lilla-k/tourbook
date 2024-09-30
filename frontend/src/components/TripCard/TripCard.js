fdd4969087a57adef0bee84890435a2c0050c870
lilla-k <kalmarlilianna@gmail.com>
2024-09-30 23:36:16 +0200

    responsive trip cards

--- frontend/src/components/TripCard/TripCard.js
+++ frontend/src/components/TripCard/TripCard.js
@@ -3,40 +3,45 @@ import { useState } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import DeleteConfirmationModal from '../DeleteConfirmationModal/DeleteConfirmationModal'
 import tripService from '../../services/tripService.js'
-import {  useOutletContext, Link } from 'react-router-dom';
+import { useOutletContext, Link } from 'react-router-dom';
 
 
-function TripCard({tripId, countryName, coverImage, startDate}){
+function TripCard({ tripId, countryName, coverImage, startDate }) {
 
-    const {setTrips, setToaster, user} = useOutletContext();
-    const [deleteModalVisible, setDeleteModalVisible]= useState(false)
+    const { setTrips, setToaster, user } = useOutletContext();
+    const [deleteModalVisible, setDeleteModalVisible] = useState(false)
 
-    async function deleteTrip(){
+    async function deleteTrip() {
         await tripService.deleteTrip(tripId);
         setToaster("trip deleted");
         const trips = await tripService.getTrips(user.uid);
         setTrips(trips);
     }
 
-    function cancelDelete(){
+    function cancelDelete() {
         setDeleteModalVisible(false);
-      }
-    
-      function deleteConfirmation(){
-        setDeleteModalVisible(true);
-      }
+    }
 
-    return(
+    function deleteConfirmation(event) {
+        setDeleteModalVisible(true);
+        event.preventDefault();
+
+    }
+
+    return (
         <div className="TripCard">
-            <Link to={`/trips/${tripId}`} className="TripCard-info">
-                {coverImage?<img src={coverImage.url} alt="Trip"/>:<div className="TripCard-img-placeholder"></div>}
+            <Link to={`/trips/${tripId}`} className="TripCard-link">
+                {coverImage ?
+                    <img className="TripCard-img" src={coverImage.url} alt="Trip" /> :
+                    <div className="TripCard-img TripCard-img-placeholder"></div>
+                }
                 <div className="TripCard-details">
                     <div className="TripCard-name">{countryName}</div>
                     <div>{new Date(startDate).toLocaleDateString("en-EN")}</div>
+                     <DeleteIcon className="TripCard-deleteIcon" onClick={deleteConfirmation} />
                 </div>
             </Link>
-            <DeleteIcon className="TripCard-deleteIcon" onClick={deleteConfirmation}/>
-            {deleteModalVisible && <DeleteConfirmationModal onDelete={deleteTrip} onCancel={cancelDelete} type="trip"/>}
+            {deleteModalVisible && <DeleteConfirmationModal onDelete={deleteTrip} onCancel={cancelDelete} type="trip" />}
         </div>
     )
 }
