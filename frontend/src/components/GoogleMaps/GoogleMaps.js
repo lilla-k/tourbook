aaab0f7da6aec47a4774e951d49784959e5a178a
lilla-k <kalmarlilianna@gmail.com>
2024-10-02 23:03:01 +0200

    map controls removed

--- frontend/src/components/GoogleMaps/GoogleMaps.js
+++ frontend/src/components/GoogleMaps/GoogleMaps.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { useState } from 'react';
 import { APIProvider, Map, Marker, InfoWindow, useMarkerRef } from '@vis.gl/react-google-maps';
-import { useNavigate, useOutletContext} from "react-router-dom";
+import { useNavigate, useOutletContext } from "react-router-dom";
 import './GoogleMaps.css';
-import MapStyle from'./MapStyle.js';
+import MapStyle from './MapStyle.js';
 import countries from '../../countries.js';
 
 
 function GoogleMaps() {
 
   const navigate = useNavigate();
-  const {trips} = useOutletContext();
+  const { trips } = useOutletContext();
 
-  function findPosition(country){
-    const countryObj=countries.find(c=> c.name===country);
-    const coordinateObj={lat:countryObj.lat, lng: countryObj.lng}
+  function findPosition(country) {
+    const countryObj = countries.find(c => c.name === country);
+    const coordinateObj = { lat: countryObj.lat, lng: countryObj.lng }
     return coordinateObj;
   }
 
@@ -23,18 +23,25 @@ function GoogleMaps() {
 
   return (
     <APIProvider apiKey={'AIzaSyBm0QjFlzIeB_Cl_e7lCMPagSRYcNkzGZI'}>
-      <Map center={{ lat: 40, lng: 10.00678 }} zoom={2.2} className="Map">
+      <Map
+        center={{ lat: 40, lng: 10.00678 }}
+        zoom={2.2}
+        fullscreenControl={false}
+        mapTypeControl={false}
+        streetViewControl={false}
+        className="Map" 
+      >
         {trips.map(trip => {
           return (
             <>
-              <Marker 
-                ref={selectedTour===trip.id ? markerRef : null} 
-                position={findPosition(trip.country)} 
-                onMouseOver={() => setSelectedTour(trip.id)} 
-                onClick={()=>navigate(`/trips/${trip.id}`)}
-                 
+              <Marker
+                ref={selectedTour === trip.id ? markerRef : null}
+                position={findPosition(trip.country)}
+                onMouseOver={() => setSelectedTour(trip.id)}
+                onClick={() => navigate(`/trips/${trip.id}`)}
+
               />
-              {selectedTour===trip.id &&
+              {selectedTour === trip.id &&
                 <InfoWindow className="Map-InfoWindow" anchor={marker} >
                   <h2 className="Map-InfoWindow-title">{trip.country}</h2>
                 </InfoWindow>}
@@ -42,7 +49,7 @@ function GoogleMaps() {
           )
         })}
       </Map>
-      <MapStyle/>
+      <MapStyle />
     </APIProvider>
   );
 }
