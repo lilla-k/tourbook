9b65d0985d432f73c028b1197b00124d14c82cea
lilla-k <kalmarlilianna@gmail.com>
2024-12-09 12:42:05 +0100

    edit profile button created

--- frontend/src/components/ProfilePage/ProfilePage.js
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -4,6 +4,7 @@ import firebaseApp from '../../services/firebase.js';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useOutletContext, Link} from 'react-router-dom';
 import PersonIcon from '@mui/icons-material/Person';
+import EditIcon from '@mui/icons-material/Edit';
 
 
 function ProfilePage(){
@@ -16,11 +17,19 @@ function ProfilePage(){
         <div className="ProfilePage">
             <div className="ProfilePage-card">
                 {user.photoURL? <img className="ProfilePage-ProfileImage" src={user.photoURL} alt={"Profile"} /> : <PersonIcon sx={{fontSize: "42px"}}/>}
-                <div className="ProfilePage-info">
-                    <div className="ProfilePage-name">{user.displayName}</div>
+                <div className="ProfilePage-details">
+                    <div className="ProfilePage-title">
+                        <div className="ProfilePage-name">{user.displayName}</div>
+                        <button className="ProfilePage-edit-icon-container">  
+                            <EditIcon fontSize="small" className="ProfilePage-edit-icon"/>
+                            Edit profile
+                        </button>
+                    </div>
                     <div>{user.email}</div>
+                    <div>Hungary</div>
                     <div>{trips.length} <Link to={`/trips`}>trips</Link></div>
                 </div>
+               
             </div>
             
         </div>
