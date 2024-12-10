d2cbc04eb4ff6f3882fab1a872bd2b93572c535a
lilla-k <kalmarlilianna@gmail.com>
2024-12-10 12:14:31 +0100

    #9 editProfilePage header created

--- frontend/src/components/EditProfilePage/EditProfilePage.js
+++ frontend/src/components/EditProfilePage/EditProfilePage.js
@@ -1,10 +1,22 @@
-
+import './EditProfilePage.css';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate, useOutletContext } from "react-router-dom";
 
 function EditProfilePage(){
 
+    const navigate = useNavigate();
+    const { user } = useOutletContext();
+
+    console.log(user.uid)
+
     return(
-        <div>
-            EDIT
+        <div className="EditProfilePage">
+            <div className="EditProfilePage-header">
+            <div className="EditProfilePage-arrowBackIcon">
+                        <ArrowBackIcon onClick={ navigate(`/users/${user.uid}`)} />
+                    </div>
+                    <div className="EditProfilePage-title">Edit your profile</div>
+            </div>
         </div>
     )
 }
