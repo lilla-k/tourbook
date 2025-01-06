7c46c82e049251a343af7783425daeff5a7273ec
lilla-k <kalmarlilianna@gmail.com>
2025-01-06 10:41:43 +0100

    #13 media query on tripPage

--- frontend/src/components/VisitedCities/VisitedCities.js
+++ frontend/src/components/VisitedCities/VisitedCities.js
@@ -5,35 +5,35 @@ import EditIcon from '@mui/icons-material/Edit';
 
 import './VisitedCities.css';
 
-function VisitedCities({selectedTrip, selectedCity}){
+function VisitedCities({ selectedTrip, selectedCity }) {
 
-    console.log(selectedTrip);
-    console.log(selectedCity);
-    const navigate = useNavigate();
+  console.log("selected trip", selectedTrip);
+  console.log("selected city", selectedCity);
+  const navigate = useNavigate();
 
-    return(
-            <div className="VisitedCities">
-          <div className="VisitedCities-title">Visited cities</div>
-          <div className="VisitedCities-container">
-            {selectedTrip.visitedCities?.map(city => {
-              return (
-                <div className={`VisitedCity ${city.cityId === selectedCity?.cityId ? `selected` : ``}`} key={city.cityId}>
-                  <Link to={`/trips/${selectedTrip.id}/${city.cityId}`} className="VisitedCityLink" >
-                    <LocationOnIcon />
-                    <div>{city.cityName}</div>
-                  </Link>
-                  <div className="VisitedCityIconEdit">
-                    <EditIcon
-                      fontSize="small"
-                      onClick={() => navigate(`/trips/${selectedTrip.id}/${city.cityId}/edit`)} />
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-          <Button onClick={() => navigate(`/trips/${selectedTrip.id}/addCity`)} variant="outlined">+ Add City</Button>
-        </div>
-    )
+  return (
+    <div className="VisitedCities">
+      <div className="VisitedCities-title">Visited cities</div>
+      <div className="VisitedCities-container">
+        {selectedTrip.visitedCities?.map(city => {
+          return (
+            <div className={`VisitedCity ${city.cityId === selectedCity?.cityId ? `selected` : ``}`} key={city.cityId}>
+              <Link to={`/trips/${selectedTrip.id}/${city.cityId}`} className="VisitedCityLink" >
+                <LocationOnIcon />
+                <div>{city.cityName}</div>
+              </Link>
+              <div className="VisitedCityIconEdit">
+                <EditIcon
+                  fontSize="small"
+                  onClick={() => navigate(`/trips/${selectedTrip.id}/${city.cityId}/edit`)} />
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <Button onClick={() => navigate(`/trips/${selectedTrip.id}/addCity`)} variant="outlined">+ Add City</Button>
+    </div>
+  )
 }
 
 export default VisitedCities
