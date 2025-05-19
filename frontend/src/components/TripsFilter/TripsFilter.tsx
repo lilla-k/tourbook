f474fe840d71ef774e9a89fc9aa03dd663074fd4
lilla-k <kalmarlilianna@gmail.com>
2025-05-19 15:28:34 +0200

    #44 filter type

--- frontend/src/components/TripsFilter/TripsFilter.tsx
+++ frontend/src/components/TripsFilter/TripsFilter.tsx
@@ -5,22 +5,39 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 import './TripsFilter.css';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 
-function TripsFilter({ yearOptions, setSelectedYear }: { yearOptions: number[], setSelectedYear: Function }) {
-  const handleChange = (event: SelectChangeEvent) => {
+function TripsFilter({
+  yearOptions, setSelectedYear, typeOptions, setSelectedType,
+}:{
+  yearOptions: number[], setSelectedYear: Function, typeOptions: string[], setSelectedType: Function,
+}) {
+  const handleFilterYear = (event: SelectChangeEvent) => {
     setSelectedYear(event.target.value as string);
+  };
+  const handleFilterType = (event: SelectChangeEvent) => {
+    setSelectedType(event.target.value as string);
   };
 
   return (
     <div className="TripsFilter">
       <FilterAltIcon className="TripsFilter-filterIcon" />
-      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }} className="TripsFilter-content">
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
         <InputLabel id="demo-simple-select-label">Year</InputLabel>
         <Select
           label="Year"
-          onChange={handleChange}
+          onChange={handleFilterYear}
         >
           <MenuItem value={undefined}>None</MenuItem>
           {yearOptions.map((year) => <MenuItem value={year}>{year}</MenuItem>)}
+        </Select>
+      </FormControl>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-label">Type</InputLabel>
+        <Select
+          label="Type"
+          onChange={handleFilterType}
+        >
+          <MenuItem value={undefined}>None</MenuItem>
+          {typeOptions.map((type) => <MenuItem value={type}>{type}</MenuItem>)}
         </Select>
       </FormControl>
     </div>
--- frontend/src/components/TripsPage/TripsPage.tsx
+++ frontend/src/components/TripsPage/TripsPage.tsx
@@ -11,16 +11,26 @@ function TripsPage() {
   const yearOptions = [...new Set(trips.map((trip) => trip.startDate.getFullYear()))];
   const [selectedYear, setSelectedYear] = useState(undefined);
 
+  const typeOptions = [...new Set(trips.map((trip) => trip.tripType))];
+  const [selectedType, setSelectedType] = useState(undefined);
+
   return (
     <div className="TripsPage">
       <div className="TripsPage-title">My trips</div>
-      <TripsFilter yearOptions={yearOptions} setSelectedYear={setSelectedYear} />
+      <TripsFilter
+        yearOptions={yearOptions}
+        setSelectedYear={setSelectedYear}
+        typeOptions={typeOptions}
+        setSelectedType={setSelectedType}
+      />
       <div className="TripsPage-container">
         {trips.length === 0
           ? <button className="TripsPage-addFirstTrip" onClick={() => navigate('/addTrip')} type="button">+ Add your first trip</button>
           : trips.filter((trip) => {
             if (selectedYear) {
               return trip.startDate.getFullYear() === selectedYear;
+            } if (selectedType) {
+              return trip.tripType === selectedType;
             }
             return trip;
           }).map((trip) => (
