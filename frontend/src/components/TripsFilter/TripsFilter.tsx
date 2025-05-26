0548741f3cb0d0fee08d06b208e5c2466bc28b20
lilla-k <kalmarlilianna@gmail.com>
2025-05-26 12:25:49 +0200

    #46 filter and sort states are in the url

--- frontend/src/components/TripsFilter/TripsFilter.tsx
+++ frontend/src/components/TripsFilter/TripsFilter.tsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from 'react-router-dom';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -7,36 +8,72 @@ import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import type { Trip } from '../../types/trip.js';
 import { getVisitedCountries, getContinent } from '../../utils/trips.js';
 
-type TripFilterState = {
-  year?: number,
-  type?: string,
-  rating?: number | null,
-  continent?: string
-};
-
 function TripsFilter({
-  trips, filter, setFilter, setSortType, location,
-} : {
-  trips: Trip[], filter: TripFilterState, setFilter: Function, setSortType: Function, location: string | undefined
+  trips, location,
+}: {
+  trips: Trip[], location: string | undefined
 }) {
-  const yearOptions = [...new Set(trips.map((trip) => trip.startDate.getFullYear()))];
-  const typeOptions = [...new Set(trips.map((trip) => trip.tripType))];
-  const ratingOptions = [...new Set(trips.map((trip) => trip.rating).filter((rating) => rating !== null))];
-  const continentOptions = [...new Set(getVisitedCountries(trips).map((c) => getContinent(c)))];
+  const [searchParams, setSearchParams] = useSearchParams();
+  const yearVariants = [...new Set(trips.map((trip) => trip.startDate.getFullYear()))];
+  const typeVariants = [...new Set(trips.map((trip) => trip.tripType))];
+  const ratingVariants = [...new Set(trips.map((trip) => trip.rating).filter((rating) => rating !== null))];
+  const continentVariants = [...new Set(getVisitedCountries(trips).map((c) => getContinent(c)))];
+  const sortVariants = ['date', 'rating', 'name', 'distance'];
+
   const handleFilterYear = (event: SelectChangeEvent) => {
-    setFilter({ ...filter, year: event.target.value });
+    const filteredYear = event.target.value;
+    setSearchParams((params: URLSearchParams) => {
+      if (filteredYear !== undefined) {
+        params.set('year', filteredYear);
+      } else {
+        params.delete('year');
+      }
+      return params;
+    });
   };
   const handleFilterType = (event: SelectChangeEvent) => {
-    setFilter({ ...filter, type: event.target.value });
+    const filteredType = event.target.value;
+    setSearchParams((params: URLSearchParams) => {
+      if (filteredType !== undefined) {
+        params.set('type', filteredType);
+      } else {
+        params.delete('type');
+      }
+      return params;
+    });
   };
   const handleFilterRating = (event: SelectChangeEvent) => {
-    setFilter({ ...filter, rating: event.target.value });
+    const filteredRating = event.target.value;
+    setSearchParams((params: URLSearchParams) => {
+      if (filteredRating !== undefined) {
+        params.set('rating', filteredRating);
+      } else {
+        params.delete('rating');
+      }
+      return params;
+    });
   };
   const handleFilterContinent = (event: SelectChangeEvent) => {
-    setFilter({ ...filter, continent: event.target.value });
+    const filteredContinent = event.target.value;
+    setSearchParams((params: URLSearchParams) => {
+      if (filteredContinent !== undefined) {
+        params.set('continent', filteredContinent);
+      } else {
+        params.delete('continent');
+      }
+      return params;
+    });
   };
   const handleSorting = (event: SelectChangeEvent) => {
-    setSortType(event.target.value);
+    const filteredSort = event.target.value;
+    setSearchParams((params: URLSearchParams) => {
+      if (filteredSort !== undefined) {
+        params.set('sortBy', filteredSort);
+      } else {
+        params.delete('sortBy');
+      }
+      return params;
+    });
   };
 
   return (
@@ -46,54 +83,60 @@ function TripsFilter({
         <InputLabel id="demo-simple-select-label">Year</InputLabel>
         <Select
           label="Year"
+          value={searchParams.get('year') || undefined}
           onChange={handleFilterYear}
         >
           <MenuItem value={undefined}>None</MenuItem>
-          {yearOptions.map((year) => <MenuItem value={year}>{year}</MenuItem>)}
+          {yearVariants.map((year) => <MenuItem value={year}>{year}</MenuItem>)}
         </Select>
       </FormControl>
       <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
         <InputLabel id="demo-simple-select-label">Type</InputLabel>
         <Select
           label="Type"
+          value={searchParams.get('type') || undefined}
           onChange={handleFilterType}
         >
           <MenuItem value={undefined}>None</MenuItem>
-          {typeOptions.map((type) => <MenuItem value={type}>{type}</MenuItem>)}
+          {typeVariants.map((type) => <MenuItem value={type}>{type}</MenuItem>)}
         </Select>
       </FormControl>
       <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
         <InputLabel id="demo-simple-select-label">Rating</InputLabel>
         <Select
           label="Rating"
+          value={searchParams.get('rating') || undefined}
           onChange={handleFilterRating}
         >
           <MenuItem value={undefined}>None</MenuItem>
-          {ratingOptions.map((rating) => <MenuItem value={rating}>{rating}</MenuItem>)}
+          {ratingVariants.map((rating) => <MenuItem value={rating}>{rating}</MenuItem>)}
         </Select>
       </FormControl>
       <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
         <InputLabel id="demo-simple-select-label">Continent</InputLabel>
         <Select
           label="Continent"
+          value={searchParams.get('continent') || undefined}
           onChange={handleFilterContinent}
         >
           <MenuItem value={undefined}>None</MenuItem>
-          {continentOptions.map((continent) => <MenuItem value={continent}>{continent}</MenuItem>)}
+          {continentVariants.map((continent) => <MenuItem value={continent}>{continent}</MenuItem>)}
         </Select>
       </FormControl>
       <div className="TripsFilter-gap" />
       <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }} className="TripsFilter-sort">
         <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
         <Select
-          label="Continent"
+          label="Sort by"
+          value={searchParams.get('sortBy') || undefined}
           onChange={handleSorting}
-          defaultValue="date"
         >
-          <MenuItem value="date">Date</MenuItem>
-          <MenuItem value="rating">Rating</MenuItem>
-          <MenuItem value="name">Country name</MenuItem>
-          {location && <MenuItem value="distance">Distance from home</MenuItem>}
+          {sortVariants.filter((sort) => {
+            if (location !== undefined) {
+              return sort !== 'distance';
+            }
+            return true;
+          }).map((sort) => <MenuItem value={sort}>{sort}</MenuItem>)}
         </Select>
       </FormControl>
     </div>
--- frontend/src/components/TripsPage/TripsPage.tsx
+++ frontend/src/components/TripsPage/TripsPage.tsx
@@ -1,5 +1,4 @@
-import { useOutletContext, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useOutletContext, useNavigate, useSearchParams } from 'react-router-dom';
 import TripCard from '../TripCard/TripCard.js';
 import './TripsPage.css';
 import TripsFilter from '../TripsFilter/TripsFilter.js';
@@ -8,40 +7,32 @@ import type Context from '../../types/context.js';
 import type { Trip } from '../../types/trip.js';
 import { getDistanceFromLocation } from '../../utils/location.js';
 
-type TripFilterState = {
-  year?: number,
-  type?: string,
-  rating?: number | null,
-  continent?: string
-};
-
 function TripsPage() {
   const navigate = useNavigate();
   const { trips, userData } = useOutletContext<Context>();
+  const [searchParams] = useSearchParams();
 
-  const [filter, setFilter] = useState<TripFilterState>({
-    year: undefined,
-    type: undefined,
-    rating: null,
-    continent: undefined,
-  });
-  const [sortType, setSortType] = useState('date');
+  const selectedYear = Number(searchParams.get('year'));
+  const selectedType = searchParams.get('type');
+  const selectedRating = Number(searchParams.get('rating'));
+  const selectedContinent = searchParams.get('continent');
+  const selectedSortType = searchParams.get('sortBy');
 
   const filteredTrips = trips.filter((trip) => (
-    (filter.year ? filter.year === trip.startDate.getFullYear() : true)
-    && (filter.type ? trip.tripType === filter.type : true)
-    && (filter.rating ? trip.rating === filter.rating : true)
-    && (filter.continent ? filter.continent === getContinent(trip.country) : true)
+    (selectedYear ? trip.startDate.getFullYear() === selectedYear : true)
+    && (selectedType ? trip.tripType === selectedType : true)
+    && (selectedRating ? trip.rating === selectedRating : true)
+    && (selectedContinent ? selectedContinent === getContinent(trip.country) : true)
   ));
 
   filteredTrips.sort((a: Trip, b: Trip) => {
-    if (sortType === 'rating') {
+    if (selectedSortType === 'rating') {
       return (b.rating || -2) - (a.rating || -2);
     }
-    if (sortType === 'name') {
+    if (selectedSortType === 'name') {
       return (a.country.localeCompare(b.country));
     }
-    if (sortType === 'distance' && userData.location?.name) {
+    if (selectedSortType === 'distance' && userData.location?.name) {
       return (getDistanceFromLocation(userData.location.name, a.country) - (getDistanceFromLocation(userData.location.name, b.country)));
     }
     return (b.startDate.getTime() - a.startDate.getTime());
@@ -52,9 +43,6 @@ function TripsPage() {
       <div className="TripsPage-title">My trips</div>
       <TripsFilter
         trips={trips}
-        filter={filter}
-        setFilter={setFilter}
-        setSortType={setSortType}
         location={userData.location?.name}
       />
       <div className="TripsPage-container">
